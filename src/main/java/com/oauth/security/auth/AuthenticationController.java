ef5ba77d46c00bee123984c2ea968a26977f9571
alexei88 <alexeidz81@gmail.com>
2023-04-18 18:53:50 +0530

    updated cross origin

--- src/main/java/com/oauth/security/auth/AuthenticationController.java
+++ src/main/java/com/oauth/security/auth/AuthenticationController.java
@@ -11,6 +11,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 public class AuthenticationController {
 
   private final AuthenticationService service;
--- src/main/java/com/oauth/security/demo/RecordController.java
+++ src/main/java/com/oauth/security/demo/RecordController.java
@@ -11,6 +11,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/auth/record")
 @AllArgsConstructor
+@CrossOrigin(origins = "*")
 public class RecordController {
 
   private final PatientRecordService service;
